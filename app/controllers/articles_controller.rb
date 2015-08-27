5f171841cc17e00afd23ca3e3c2606fea115dbcb
leVirve <gae.m.project@gmail.com>
2015-08-27 16:21:45 +0800

    refactor to style in eddie's tutorial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,12 +2,13 @@ class ArticlesController < ApplicationController
 
     http_basic_authenticate_with name: "sasa", password: "sss", except: [:index, :show]
 
+    before_action :find_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -15,37 +16,37 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create
         @article = Article.new(article_params)
 
         if @article.save
-            redirect_to @article
+            redirect_to @article, notice: '新增成功'
         else
             render :new
         end 
     end
 
     def update
-        @article = Article.find(params[:id])
-
         if @article.update(article_params)
-            redirect_to @article
+            redirect_to @article, notice: '更新成功'
         else
             render :edit
         end 
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
-
-        redirect_to articles_path
+        redirect_to articles_path, notice: '已刪除'
     end
 
     private
+        def find_article
+            @article = Article.find_by(id: params[:id])
+            redirect_to articles_path, notice: '無此篇文章' if not @article
+        end
+
         def article_params
             params.require(:article).permit(:title, :text)
         end
